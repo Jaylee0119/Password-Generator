50030cbc8c53be9eaab149309b36b84baacf3b6f
Jaylee0119 <jaleesalee@protonmail.com>
2022-02-28 13:26:46 +0300

    prompt user for lenght

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,23 @@ var confirmNumber;
 var confirmSpecial;
 var userChoices;
 
+var passwordLength; // check length of characters from user input
+var confirmLower; //confirms if user wants lowercase letters
+var confirmUpper; //confirms if user wants uppercase letters
+var confirmNumber; //ask user if to include numbers in the password to be generated
+var confirmSpecial; //confirm special characters
+var choice_by_user;  //choices from user
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+@@ -25,3 +23,10 @@ function writePassword() {
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Start Function
+function generatePassword() {
+  // Ask four user Input
+  passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + passwordLength);
+}; 
